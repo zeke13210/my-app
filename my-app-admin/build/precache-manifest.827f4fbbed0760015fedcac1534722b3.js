42014b6dd4e7292379ea0761750e3e0baeeb0b54
zeke13210 <zfarley94@gmail.com>
2020-01-24 08:31:07 -0500

    update ignore and add build/

---
+++ my-app-admin/build/precache-manifest.827f4fbbed0760015fedcac1534722b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbead0be06817222246ec925faefa0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f461fc42436d2049c9b7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ab808324682bedf25dde",
+    "url": "/static/js/2.e9c6bc1c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e9c6bc1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f461fc42436d2049c9b7",
+    "url": "/static/js/main.9aa3cdf7.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
